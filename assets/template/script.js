38bf26af7868fc539256841d4c2f8d030a777b34
kaplego <kaplego@sharme.eu>
2021-11-27 10:40:01 +0100

    Added features section

--- assets/template/script.js
+++ assets/template/script.js
@@ -2,7 +2,16 @@ $('.show-up').animateVisible({ up: true });
 $('.show-up-small').animateVisible({ up: true });
 $('.show-down').animateVisible({ up: true });
 
-window.addEventListener("scroll", event => {
+[...$('.show-up, .show-up-small, .show-down')].forEach(element => {
+    element.classList.remove('show-end');
+    element.addEventListener('animationend', () => {
+        element.classList.add('show-end');
+    });
+});
+
+window.onscroll = headerScroll;
+
+function headerScroll() {
     if (window.scrollY > 0) {
         $('#header')[0].classList.remove("bg-transparent", "navbar-dark");
         $('#header')[0].classList.add("bg-light", "navbar-light", "shadow-bottom");
@@ -14,7 +23,7 @@ window.addEventListener("scroll", event => {
         $('#header .navbar-brand img')[0].src = "/assets/images/bootstrap-logo-white.svg";
         $('#nav-separator')[0].classList.add("top");
     }
-});
+}
 
 $('a[href^="\#"]').on("click", event => {
     event.preventDefault();
@@ -27,7 +36,7 @@ $('a[href^="\#"]').on("click", event => {
 
 $(document).ready(function() {
     window.innerWidth <= 768 ? $(document.body).scrollspy({ target: ".scrollspy2" }) : $(document.body).scrollspy({ target: ".scrollspy" });
-    window.onscroll;
+    headerScroll();
 
     if (window.location.hash && window.location.hash != "") {
         $(window.location.hash)[0].scrollIntoView({ behavior: 'smooth', block: 'start' });
